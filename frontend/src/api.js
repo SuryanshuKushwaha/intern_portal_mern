bec6f0272306071dc46ca9fad6586020ed9abafd
Suryanshu Kushwaha <114632428+SuryanshuKushwaha@users.noreply.github.com>
2025-08-21 22:18:06 +0530

    Update api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -2,5 +2,6 @@ import axios from 'axios';
 
 const API_BASE = process.env.REACT_APP_API_URL || 'http://localhost:5000';
 
-export const api = axios.create({ baseURL: API_BASE + '/api'});
+export const api = axios.create({ baseURL: API_BASE + '/api/applicants'});
+
 
