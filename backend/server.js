cc49782662b25e9e8483116035ff9d7023d7cdd8
Suryanshu Kushwaha <114632428+SuryanshuKushwaha@users.noreply.github.com>
2025-08-22 15:44:49 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,7 @@ mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopol
   .then(() => console.log('MongoDB connected'))
   .catch(err => console.error('MongoDB connection error', err));
 
-app.use('/', applicantsRoutes);
+app.use('/api/applicants', applicantsRoutes);
 
 app.get('/', (req, res) => res.json({ status: 'ok' }));
 
